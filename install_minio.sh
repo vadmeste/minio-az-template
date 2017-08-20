dcf77bf29133daee88fb74576857dfa5dd75823a
Anis Elleuch <anis@minio.io>
2017-08-20 14:11:29 +0100

    Run Minio server in Azure gateway mode

--- install_minio.sh
+++ install_minio.sh
@@ -3,4 +3,7 @@
 curl 'https://dl.minio.io/server/minio/release/linux-amd64/minio' > /usr/bin/minio
 chmod +x /usr/bin/minio
 
-/usr/bin/minio &
+export MINIO_ACCESS_KEY="$1"
+export MINIO_SECRET_KEY="$2"
+
+/usr/bin/minio gateway azure &
