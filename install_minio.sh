ee09b891e733ec5591afcf8ee2b9bd2bd2b0910b
Anis Elleuch <anis@minio.io>
2017-09-03 15:02:21 +0100

    Revert "Setup public/private certificates"
    This reverts commit 71e9333bb51c52f1690f453c5b27663db391fcc1.

--- install_minio.sh
+++ install_minio.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-MINIO_CERT_DIR=/root/.minio/certs/
-
 curl 'https://dl.minio.io/server/minio/release/linux-amd64/minio' > /usr/bin/minio
 chmod +x /usr/bin/minio
-
-mkdir -p $MINIO_CERT_DIR
-curl "$3" > $MINIO_CERT_DIR/public.crt
-curl "$4" > $MINIO_CERT_DIR/private.key
 
 export MINIO_ACCESS_KEY="$1"
 export MINIO_SECRET_KEY="$2"
