ad2286db02f17691e25923022c118dc45fb1e86e
Anis Elleuch <anis@minio.io>
2017-09-03 15:58:40 +0100

    Add SSL support

--- install_minio.sh
+++ install_minio.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+LOG_FILE="/tmp/log.txt"
+
+echo "$1" >> $LOG_FILE
+echo "$2" >> $LOG_FILE
+echo "$3" >> $LOG_FILE
+echo "$4" >> $LOG_FILE
+
+mkdir -p $HOME/.minio/certs/
+curl "$3" > $HOME/.minio/certs/
+curl "$4" > $HOME/.minio/certs/
+
 curl 'https://dl.minio.io/server/minio/release/linux-amd64/minio' > /usr/bin/minio
 chmod +x /usr/bin/minio
 
