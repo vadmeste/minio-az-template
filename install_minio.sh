5050e7ebc8b4d4879961574a598120811c6cbfd4
Anis Elleuch <anis@minio.io>
2017-09-04 22:25:13 +0100

    Use systemd to start Minio

--- install_minio.sh
+++ install_minio.sh
@@ -2,21 +2,45 @@
 
 LOG_FILE="/tmp/log.txt"
 
-echo "$1" >> $LOG_FILE
-echo "$2" >> $LOG_FILE
-echo "$3" >> $LOG_FILE
-echo "$4" >> $LOG_FILE
+MINIO_USER="$1"
+MINIO_ACCESS_KEY="$2"
+MINIO_SECRET_KEY="$3"
+PUBLIC_CERT_BLOB="$4"
+PRIVATE_CERT_BLOB="$5"
 
-HOME=/root/
+HOME="/home/$MINIO_USER"
+
+MINIO_BINARY=/usr/local/bin/minio
+SYSTEMD_CONFDIR=/etc/systemd/system/
+SYSTEMD_MINIO=https://raw.githubusercontent.com/vadmeste/minio-az-template/minio_ssl_systemd/minio.service 
+
+
+cat <<EOT > /etc/default/minio
+# Use if you want to run Minio on a custom port.
+MINIO_OPTS="--address :9000"
+EOT
+
+cat <<EOT >> /etc/default/minio
+# Access Key of the server.
+MINIO_ACCESS_KEY="$MINIO_ACCESS_KEY"
+# Secret key of the server.
+MINIO_SECRET_KEY="$MINIO_SECRET_KEY"
+
+EOT
 
 mkdir -p $HOME/.minio/certs/
-echo "$3" | base64 -d > $HOME/.minio/certs/public.crt
-echo "$4" | base64 -d > $HOME/.minio/certs/private.key
+if [ ! -z "$PUBLIC_CERT_BLOB" -o ! -z "$PRIVATE_CERT_BLOB" ]; then
+    echo "$PUBLIC_CERT_BLOB" | base64 -d > $HOME/.minio/certs/public.crt
+    echo "$PRIVATE_CERT_BLOB" | base64 -d > $HOME/.minio/certs/private.key
+fi
 
-curl 'https://dl.minio.io/server/minio/release/linux-amd64/minio' > /usr/bin/minio
-chmod +x /usr/bin/minio
+curl 'https://dl.minio.io/server/minio/release/linux-amd64/minio' > $MINIO_BINARY
+chmod +x $MINIO_BINARY
 
-export MINIO_ACCESS_KEY="$1"
-export MINIO_SECRET_KEY="$2"
+curl $SYSTEMD_MINIO > $SYSTEMD_CONFDIR/minio.service
 
-/usr/bin/minio gateway azure &
+sed -i "s#User=minio-user#User=$MINIO_USER#" $SYSTEMD_CONFDIR/minio.service
+sed -i "s#Group=minio-user#Group=$MINIO_USER#" $SYSTEMD_CONFDIR/minio.service
+
+systemctl enable minio.service
+
